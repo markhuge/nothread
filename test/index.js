f5f777b0aa915b234c00dfdac3b1e71b38369016
Mark Wilkerson <mark@markwilkerson.me>
2017-08-31 16:58:46 -0700

    update tests with tap

--- test/index.js
+++ test/index.js
@@ -1,22 +1,40 @@
-var test     = require('tape'),
-    nothread = require('../');
+const test = require('tap').test;
+const nothread = require('../');
 
-test('object headers', function (t) {
-  t.plan(2);
- 
-  var msg    = { headers: { foo: "bar" }, subject: "some subject" },
-      result = nothread(msg);
+test('object headers', assert => {
+	const msg = {headers: {foo: 'bar'}, subject: 'some subject'};
+	const result = nothread(msg);
 
-  t.ok(result.subject.match(/^some subject \- [a-f0-9]{20}$/));
-  t.ok(result.headers['In-Reply-To'].match(/^<[a-f0-9]{20}>$/));
+	assert.ok(result.subject.match(/^some subject - [a-f0-9]{20}$/));
+	assert.ok(result.headers['In-Reply-To'].match(/^<[a-f0-9]{20}>$/));
+	assert.end();
 });
 
-test('text headers', function (t) {
-  t.plan(2);
+test('string headers', assert => {
+	const msg = {headers: 'foo: bar', subject: 'some subject'};
+	const result = nothread(msg);
 
-  var msg    = { headers: "foo: bar", subject: "some subject" },
-      result = nothread(msg);
+	assert.ok(result.subject.match(/^some subject - [a-f0-9]{20}$/));
+	assert.ok(result.headers.match(/foo: bar\nIn-Reply-To: <[a-f0-9]{20}>/));
+	assert.end();
+});
 
-  t.ok(result.subject.match(/^some subject \- [a-f0-9]{20}$/));
-  t.ok(result.headers.match(/foo: bar\nIn-Reply-To: <[a-f0-9]{20}>/));
+test('invalid subject', assert => {
+	const msg = {subject: 1};
+	assert.throws(() => nothread(msg), new TypeError('Invalid mail object: {"subject":1}'));
+	assert.end();
+});
+
+test('invalid obj', assert => {
+	const msg1 = 'test';
+	assert.throws(() => nothread(msg1), new TypeError('Invalid mail object: "test"'));
+	const msg2 = undefined;
+	assert.throws(() => nothread(msg2), new TypeError('Invalid mail object: undefined'));
+	assert.end();
+});
+
+test('missing headers', assert => {
+	const msg = {subject: 'test'};
+	assert.throws(() => nothread(msg), 'Throw on missing headers');
+	assert.end();
 });
