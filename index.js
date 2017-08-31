1e4eddab95dd5acbc84d4b43a2eb3cdc46f2203f
Mark Wilkerson <mark@markwilkerson.me>
2017-08-31 16:59:07 -0700

    modernize

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-var crypto = require('crypto');
+const crypto = require('crypto');
 
-module.exports = function (obj) {
+module.exports = obj => {
+	if (
+		typeof obj === 'object' &&
+		typeof obj.subject === 'string'
+	) {
+		return parse(obj);
+	}
 
-    var pad = crypto.randomBytes(10).toString('hex');
-    obj.subject += ' - ' + pad;
-  
-    if (obj.headers && typeof obj.headers === 'object') {
-        obj.headers['In-Reply-To'] =  "<" + pad + ">";
-    } else {
-      obj.headers = obj.headers || "";
-      obj.headers += "\nIn-Reply-To: <" + pad + ">";
-    }
-    return obj;
+	return errorHandler(obj);
+};
+
+function parse(obj) {
+	const pad = crypto.randomBytes(10).toString('hex');
+	obj.subject += ` - ${pad}`;
+
+	if (!obj.headers) {
+		return errorHandler(obj);
+	}
+
+	if (typeof obj.headers === 'object') {
+		obj.headers['In-Reply-To'] = '<' + pad + '>';
+	}
+
+	if (typeof obj.headers === 'string') {
+		obj.headers += '\nIn-Reply-To: <' + pad + '>';
+	}
+
+	return obj;
+}
+
+function errorHandler(obj) {
+	const msg = JSON.stringify(obj);
+	throw new TypeError(`Invalid mail object: ${msg}`);
 }
